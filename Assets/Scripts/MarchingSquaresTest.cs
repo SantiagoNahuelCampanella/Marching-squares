79a855e312c823ddd6f829ea8bf78dcfbf616567
SantiagoNahuelCampanella <110130666+SantiagoNahuelCampanella@users.noreply.github.com>
2022-10-26 13:47:10 -0300

    Base Logic Implemented
    The code creates a point grid and draws gizmo lines to represent triangles.
    Triangulation table is incomplete

--- Assets/Scripts/MarchingSquaresTest.cs
+++ Assets/Scripts/MarchingSquaresTest.cs
@@ -31,19 +31,21 @@ public class MarchingSquaresTest : MonoBehaviour
 
 
         for (int y = 0; y < sizeH - 1; y++)
+        {
+            for (int x = 0; x < sizeW - 1; x++)
             {
-                for (int x = 0; x < sizeW - 1; x++)
-                {
-                    int caseValue = 0;
+                int caseValue = 0;
                 
-                    if (points[y, x] > 0.5f) { caseValue |= 1; }
-                    if (points[y, x + 1] > 0.5f) { caseValue |= 2; }
-                    if (points[y + 1, x + 1] > 0.5f) { caseValue |= 4; }
-                    if (points[y + 1, x] > 0.5f) { caseValue |= 8; }
+                //squares are constructed clockwise, to match unity mesh renderer
+                //que "saquareCase" is constructed in a binary format, with numbers 0-15 possible
+                if (points[y, x] > 0.5f) { caseValue |= 1; }
+                if (points[y + 1, x] > 0.5f) { caseValue |= 2; }
+                if (points[y + 1, x + 1] > 0.5f) { caseValue |= 4; }
+                if (points[y, x + 1] > 0.5f) { caseValue |= 8; }
 
-                    squareCase.Add(caseValue);
-                }
+                squareCase.Add(caseValue);
             }
+        }
 
     }
 
@@ -65,19 +67,23 @@ public class MarchingSquaresTest : MonoBehaviour
                     Gizmos.DrawWireSphere(new Vector3(x, 0, y) + transform.position, 0.05f);
                 }
             }
+
+            HandleSquareCase(squareCase[0], 0, 0);
         }
     }
 
-    void HandleSquareCase(int squareCase, int startPosX , int startPosY) {
+    void HandleSquareCase(int sCase, int startPosX , int startPosY) {
         int[,] triangulationTable;
 
         Vector3[] pointPos = new Vector3[4];
 
-        pointPos[0] = new Vector3(startPosY, 0, startPosX);
-        pointPos[1] = new Vector3(startPosY, 0, startPosX + 1);
-        pointPos[2] = new Vector3(startPosY + 1, 0, startPosX + 1);
-        pointPos[3] = new Vector3(startPosY + 1, 0, startPosX);
+        //assigning a position on 3D space to the points that make up the selected square
+        pointPos[0] = new Vector3(startPosX, 0, startPosY);
+        pointPos[1] = new Vector3(startPosX, 0, startPosY + 1);
+        pointPos[2] = new Vector3(startPosX + 1, 0, startPosY + 1);
+        pointPos[3] = new Vector3(startPosX + 1, 0, startPosY);
 
+        //list of possible triangle configurations acording to the square case (16)
         triangulationTable = new int[16,18] 
         { 
             {-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1}, //0 
@@ -113,9 +119,47 @@ public class MarchingSquaresTest : MonoBehaviour
             {-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1}  //15
         };
 
-        for (int i = 0; i < 16; i += 2)
+        //every 6 numbers on the table represent 1 triangle
+        for (int i = 0; i < 16; i += 6)
         {
-            //Gizmos.DrawLine();
+            if (triangulationTable[sCase, i] != -1)
+            {
+                Vector3 vertexA = new Vector3();
+                Vector3 vertexB = new Vector3();
+                Vector3 vertexC = new Vector3();
+
+                if (triangulationTable[sCase, i + 1] != -1)
+                {
+                    vertexA = Vector3.Lerp(pointPos[triangulationTable[sCase,i]], pointPos[triangulationTable[sCase, i + 1]], 0.5f);
+                }
+                else 
+                {
+                    vertexA = pointPos[triangulationTable[sCase, i]];
+                }
+
+                if (triangulationTable[sCase, i + 3] != -1)
+                {
+                    vertexB = Vector3.Lerp(pointPos[triangulationTable[sCase, i + 2]], pointPos[triangulationTable[sCase, i + 3]], 0.5f);
+                }
+                else
+                {
+                    vertexB = pointPos[triangulationTable[sCase, i + 2]];
+                }
+
+                if (triangulationTable[sCase, i + 5] != -1)
+                {
+                    vertexC = Vector3.Lerp(pointPos[triangulationTable[sCase, i + 4]], pointPos[triangulationTable[sCase, i + 5]], 0.5f);
+                }
+                else
+                {
+                    vertexC = pointPos[triangulationTable[sCase, i + 4]];
+                }
+
+                Gizmos.color = Color.green;
+                Gizmos.DrawLine(vertexA,vertexB);
+                Gizmos.DrawLine(vertexB, vertexC);
+                Gizmos.DrawLine(vertexC, vertexA);
+            }
         }
     }
 }
